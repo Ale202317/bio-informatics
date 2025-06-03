6210e87e48c1741fbe51570a15aba3b3d13e6d06
Alessandra Bontempi <133512150+Ale202317@users.noreply.github.com>
2025-06-03 16:39:46 +0200

    Update analysis.r

--- analysis.r
+++ analysis.r
@@ -8,11 +8,5 @@ genes <- unique(data$level_1)
 database <- c("KEGG_2021_Human", "GO_Biological_Process_2021")
 enriched <- enrichr(genes, database)
 
-cat("\n--- KEGG ---\n")
-print(head(enriched[["KEGG_2021_Human"]]))
-
-cat("\n--- GO Biological Process ---\n")
-print(head(enriched[["GO_Biological_Process_2021"]]))
-
 write.csv(enriched[["KEGG_2021_Human"]], "KEGG_enrichment.csv", row.names = FALSE)
 write.csv(enriched[["GO_Biological_Process_2021"]], "GO_BP_enrichment.csv", row.names = FALSE)
